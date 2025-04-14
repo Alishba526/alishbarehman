b394a74b3c27722a22eecee9c8d356b12cacf477
Alishba526 <alishbarehman526@gmail.com>
2025-04-15 02:44:35 +0500

    fix: syncing repo

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,11 +1,9 @@
-import { FaArrowUp } from 'react-icons/fa'; 
 import './footer.css'
 function Footer() {
     return (
         <footer>
             <p>&copy; 2024 Your Website. All rights reserved.</p>
             <a href="#home" className="back-to-top">
-                <FaArrowUp />
             </a>
         </footer>
     );
